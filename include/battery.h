d83fdbeb2fbc7081f301b01bbafcaa67ebd39859
damien <lesdmart@gmail.com>
2026-02-16 15:36:10 +0100

    [CHG]  update comments

--- include/battery.h
+++ include/battery.h
@@ -11,7 +11,7 @@ void init_module_battery(void);
 
 /**
  * @brief Effectue une mesure ADC et calcule le niveau de charge.
- * Utilise une calibration pour 2 piles Li-Ion (6.0V - 8.4V).
+ * Calibration pour 2 piles Li-Ion (6.0V - 8.4V).
  * * @return int Pourcentage de battery (0 à 100), ou -1 en cas d'erreur.
  */
 int get_battery_percentage(void);
